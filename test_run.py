5ce5df00648c6c7daf9eb781ecb059a0fd9cd4d5
north14 <fandersson50@gmail.com>
2020-03-13 13:24:51 +0100

    fixed pytest

--- test_run.py
+++ test_run.py
@@ -21,7 +21,7 @@ def test_ticker_chartdata():
 
 
 def test_search():
-    search = avanza.Search("msft").info()
+    search = avanza.Search("msft").info
     for resultGroup in search['resultGroups']:
         if resultGroup['instrumentType'] == "STOCK":
             for hit in resultGroup['hits']:
